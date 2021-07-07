c5960d6518380bf580f06329e7e40ea5d14d6c27
Jesus-Rojas <jarojas6524@misena.edu.co>
2021-07-06 23:02:29 -0500

    Subiendo Archivos de Test a Git

---
+++ README.md
@@ -0,0 +1,3 @@
+# Proyecto de Fernando Herrera Udemy
+
+En este repositorio es explicito para recordar el uso de input y output en Angular.
---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { DbzModule } from './dbz/dbz.module';
+
+import { AppComponent } from './app.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    DbzModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
